c3d694dea51dfb87c8c5da563f76cfd82099c943
unknown <nguyentrongan010@gmail.com>
2022-04-12 16:02:54 +0700

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.send(`<h1>I'm still working</h1>`);
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  const messages = [
+    "Hello",
+    "Hi",
+    "How are you?",
+    "I'm fine",
+    "Thank you",
+    "Bye",
+    "See you",
+    "Goodbye",
+    "See you later",
+    "What the hell are you doing here?",
+    "I'm not doing anything",
+    "Really?",
+    "You're not doing anything",
+  ];
+  const users = [
+    { name: "John", color: "#CFD3CD" },
+    { name: "Jane", color: "#642424" },
+    { name: "Jack", color: "#7E7B52" },
+    { name: "Jill", color: "#20214F" },
+  ];
+
+  const sendRandomMessage = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    const user = users[Math.floor(Math.random() * users.length)];
+    socket.emit("chat-message", { type: "chat-message", body: message, user });
+
+    const min = 300;
+    const max = 2000;
+    const rand = Math.floor(Math.random() * (max - min + 1) + min);
+    setTimeout(sendRandomMessage, rand);
+  }
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
